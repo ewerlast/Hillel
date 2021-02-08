3d398927af634618c42a0452c99ab326d04708c4
ewerlast1984 <ewerlast1984@gmail.com>
2021-02-08 19:29:09 +0200

    homework update

--- homework1/js/javascript.js
+++ homework1/js/javascript.js
@@ -3,38 +3,35 @@
 ответил "Юра" и "1989" то выводится "Юра 28"*/
 
 // проверка на символ
-let name = prompt('what is you name?');
+let name = prompt('Как Ваше имя?');
 if (!isNaN(name)) {
   alert('введите имя');
-  name = prompt('what is you name?');
+  name = prompt('Как Ваше имя?');
 }
 // проверка на число  
-let age = prompt('How old are you?');
+let age = prompt('Введите год рождения');
 if (isNaN(age)) {
   alert("введите дату");
-  age = prompt('How old are you?');
+  age = prompt('Введите год рождения?');
 }
-alert(name + ":" + age);
+/* так бы я сделал
+
+let data =new Date();
+let thisYear = data.getFullYear();
+alert(name + ":" + (thisYear - age));
+
+*/
+alert(name + ":" + (2021 - age));
 
 
 /*2 .Ввести с клавиатуры 3 переменные (a, b, c). Привести их все в число.
  Выполнить операцию
  суммирования всех переменных. Указать какие переменные являются чётными.*/
 
-//Находими елементы DOM
+
 let a = Number(prompt("введите a:"));
 let b = Number(prompt("введите b:"));
 let c = Number(prompt("введите c:"));
-
-
-
-
-
-
-
-
-
-
 
 //проверка численного символа
 
@@ -67,14 +64,25 @@ if (isNaN(a || b || c)) {
 
 //Разбиваем число на символы
 
-let originalNumber = 12345;
+//Наверное бред ... но я вижу так
+let a1 = 12345 % 10;
+let b1 = 1234 % 10;
+let c1 = 123 % 10;
+let d1 = 12 % 10;
+let x1 = 1 % 10;
+//т.к. Я прибавляю пробел то число становится все равно строкой и Number() 
+//уже не поможет NaN
+console.log("прамой порядок:" + a1 + ' ' + b1 + ' ' + c1 + ' ' + d1 + ' ' + x1);
+console.log('обратный порядок:' + x1 + ' ' + d1 + ' ' + c1 + ' ' + b1 + ' ' + a1);
+
+
 
 // преобразуем в сроку
-originalNumber = String(originalNumber);
-console.log(originalNumber);
+//originalNumber = String(originalNumber);
+//console.log(originalNumber);
 
 //делаем массив 
-let arr2 = originalNumber.split('');
+//let arr2 = originalNumber.split('');
 
 // перебором что то пошло не так
 
@@ -85,20 +93,20 @@ let arr2 = originalNumber.split('');
     });*/
 
 //Делаю в ручную  
-arr2[0] = originalNumber[0] + " ";
-arr2[1] = originalNumber[1] + " ";
-arr2[2] = originalNumber[2] + " ";
-arr2[3] = originalNumber[3] + " ";
-arr2[4] = originalNumber[4] + " ";
+//arr2[0] = originalNumber[0] + " ";
+//arr2[1] = originalNumber[1] + " ";
+//arr2[2] = originalNumber[2] + " ";
+//arr2[3] = originalNumber[3] + " ";
+//arr2[4] = originalNumber[4] + " ";
 
 //опять в строку
-arr2.join();
-let arr3 = arr2.join();
+//arr2.join();
+//let arr3 = arr2.join();
 
 //Удаляю ","
 // Регулярные выражения знаю плохо,
 //поэcdтому ответ нашел на Хабр   
-console.log(arr3.replace(/[, ]+/g, " "));
+
 
 
 
